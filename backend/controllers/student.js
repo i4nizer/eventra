224d8b99c9f857f21d7331aa6126852aadc4b376
i4nizer <iansandoval264@gmail.com>
2025-09-19 04:20:03 +0800

    Created student get handler.

--- backend/controllers/student.js
+++ backend/controllers/student.js
@@ -1,7 +1,29 @@
+import z from "zod"
 import models from "../models/index.js"
 import schema from "../schema/index.js"
+import { Op } from "sequelize"
 
 //
+
+const get = async (req, res) => {
+    const parser = schema.pagination.PaginationSchema.extend({ id: z.coerce.number().optional() })
+    const parsed = parser.safeParse(req.query)
+    if (!parsed.success) return res.status(400).send(parsed.error.issues.at(0)?.message)
+    
+    const filter = {
+        ...(parsed.data.id && { id: parsed.data.id }),
+        ...(parsed.data.alpha && { createdAt: { [Op.gte]: new Date(parsed.data.alpha) } }),
+        ...(parsed.data.omega && { createdAt: { [Op.gte]: new Date(parsed.data.omega) } }),
+    }
+
+    const students = await models.student.Student.findAll({
+        where: filter,
+        limit: parsed.data.limit,
+        offset: parsed.data.offset,
+    })
+
+    res.json(students.map((s) => s.dataValues))
+}
 
 const post = async (req, res) => {
     const parsed = schema.student.StudentCreateSchema.safeParse(req.body)
@@ -13,4 +35,4 @@ const post = async (req, res) => {
 
 //
 
-export default { post }
+export default { get, post }
