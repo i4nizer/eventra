513445dd10480278da8f0b40acd6ae65f6eac71f
i4nizer <iansandoval264@gmail.com>
2025-09-19 03:48:59 +0800

    Created section get route.

--- backend/routes/section.js
+++ backend/routes/section.js
@@ -6,6 +6,7 @@ import middlewares from "../middlewares/index.js"
 
 const section = express.Router()
 section.route("/")
+    .get(middlewares.auth.access, controllers.section.get)
     .post(middlewares.auth.access, controllers.section.post)
     .patch(middlewares.auth.access, controllers.section.patch)
     .delete(middlewares.auth.access, controllers.section.destroy)
