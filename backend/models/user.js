6e7acc4581e61e97a2a914b319cec0450bfd5a85
i4nizer <iansandoval264@gmail.com>
2025-09-18 23:27:27 +0800

    Created user model.

---
+++ backend/models/user.js
@@ -0,0 +1,51 @@
+import { Model, DataTypes } from "sequelize"
+
+//
+
+class User extends Model {}
+
+const attr = {
+	id: {
+		type: DataTypes.INTEGER,
+		primaryKey: true,
+		autoIncrement: true,
+	},
+	name: {
+		type: DataTypes.STRING,
+		allowNull: false,
+	},
+	email: {
+		type: DataTypes.STRING,
+		allowNull: false,
+		unique: "email",
+	},
+	phone: {
+		type: DataTypes.STRING,
+		allowNull: false,
+		defaultValue: "",
+	},
+	password: {
+		type: DataTypes.STRING,
+		allowNull: false,
+	},
+	createdAt: {
+		type: DataTypes.DATE,
+		allowNull: false,
+		defaultValue: DataTypes.NOW,
+	},
+	updatedAt: {
+		type: DataTypes.DATE,
+		allowNull: false,
+		defaultValue: DataTypes.NOW,
+	},
+}
+
+const opts = (sequelize) => ({
+	sequelize,
+	tableName: "users",
+	timestamps: true,
+})
+
+//
+
+export default { User, attr, opts }
