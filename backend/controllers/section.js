27d3128d299a02d8aff7fe7d4f982a937e11608a
i4nizer <iansandoval264@gmail.com>
2025-09-19 03:48:33 +0800

    Created section get handler.

--- backend/controllers/section.js
+++ backend/controllers/section.js
@@ -1,7 +1,29 @@
+import z from "zod"
 import models from "../models/index.js"
 import schema from "../schema/index.js"
+import { Op } from "sequelize"
 
 //
+
+const get = async (req, res) => {
+    const parser = schema.pagination.PaginationSchema.extend({ id: z.coerce.number().optional() })
+    const parsed = parser.safeParse(req.query)
+    if (!parsed.success) return res.status(400).send(parsed.error.issues.at(0)?.message)
+    
+    const filter = {
+        ...(parsed.data.id && { id: parsed.data.id }),
+        ...(parsed.data.alpha && { createdAt: { [Op.gte]: new Date(parsed.data.alpha) } }),
+        ...(parsed.data.omega && { createdAt: { [Op.gte]: new Date(parsed.data.omega) } }),
+    }
+
+    const sections = await models.section.Section.findAll({
+        where: filter,
+        limit: parsed.data.limit,
+        offset: parsed.data.offset,
+    })
+
+    req.json(sections.map((s) => s.dataValues))
+}
 
 const post = async (req, res) => {
     const parsed = schema.section.SectionCreateSchema.safeParse(req.body)
@@ -35,4 +57,4 @@ const destroy = async (req, res) => {
 
 //
 
-export default { post, patch, destroy }
+export default { get, post, patch, destroy }
