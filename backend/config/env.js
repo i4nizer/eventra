01f16d05a2d5e335c6e6357357c78a406bbf8d1c
i4nizer <iansandoval264@gmail.com>
2025-09-19 00:58:29 +0800

    Added jwt on env.

--- backend/config/env.js
+++ backend/config/env.js
@@ -7,7 +7,9 @@ const env = {
 	port: Number(process.env.PORT),
 	sequelizeUri: process.env.SEQUELIZE_URI,
 	sequelizeSync: !!process.env.SEQUELIZE_SYNC,
-	sequelizeLogging: !!process.env.SEQUELIZE_LOGGING,
+    sequelizeLogging: !!process.env.SEQUELIZE_LOGGING,
+    jwtAccessLife: Number(process.env.JWT_ACCESS_LIFE),
+    jwtAccessSecret: process.env.JWT_ACCESS_SECRET,
 }
 
 //
