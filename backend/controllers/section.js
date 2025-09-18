71ee8b199b3ec4c538c5579db576bcf62c4e815c
i4nizer <iansandoval264@gmail.com>
2025-09-19 03:16:17 +0800

    Created section patch handler.

--- backend/controllers/section.js
+++ backend/controllers/section.js
@@ -11,6 +11,17 @@ const post = async (req, res) => {
     res.json(section.dataValues)
 }
 
+const patch = async (req, res) => {
+    const parsed = schema.section.SectionUpdateSchema.safeParse(req.body)
+    if (!parsed.success) return res.status(400).send(parsed.error.issues.at(0)?.message)
+    
+    const section = await models.section.Section.findOne({ where: { id: parsed.data.id } })
+    if (!section) return res.status(404).send("Section not found.")
+
+    await section.update({ ...parsed.data })
+    res.json(section.dataValues)
+}
+
 //
 
-export default { post }
+export default { post, patch }
