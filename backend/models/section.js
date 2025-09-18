a721b8a471b85307fda66538149384f9dc19d774
i4nizer <iansandoval264@gmail.com>
2025-09-19 03:07:35 +0800

    Created section model.

---
+++ backend/models/section.js
@@ -0,0 +1,41 @@
+import { Model, DataTypes } from "sequelize"
+
+//
+
+class Section extends Model {}
+
+const attr = {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    year: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+    },
+}
+
+const opts = (sequelize) => ({
+    sequelize,
+    tableName: "sections",
+    timestamps: true,
+})
+
+//
+
+export default { Section, attr, opts }
