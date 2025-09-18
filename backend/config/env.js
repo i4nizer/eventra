9db8c9112fd72308c6b1267e93c14b271d9a5094
i4nizer <iansandoval264@gmail.com>
2025-09-19 00:38:41 +0800

    Added port on env.

--- backend/config/env.js
+++ backend/config/env.js
@@ -4,9 +4,10 @@ dotenv.config({ quiet: true })
 //
 
 const env = {
-    sequelizeUri: process.env.SEQUELIZE_URI,
-    sequelizeSync: !!process.env.SEQUELIZE_SYNC,
-    sequelizeLogging: !!process.env.SEQUELIZE_LOGGING,
+	port: Number(process.env.PORT),
+	sequelizeUri: process.env.SEQUELIZE_URI,
+	sequelizeSync: !!process.env.SEQUELIZE_SYNC,
+	sequelizeLogging: !!process.env.SEQUELIZE_LOGGING,
 }
 
 //
