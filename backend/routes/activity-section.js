237fe38b7ecefd90193ed4ea06c36c091c16a36d
i4nizer <iansandoval264@gmail.com>
2025-10-26 01:10:44 +0800

    Created activity-section api route.

---
+++ backend/routes/activity-section.js
@@ -0,0 +1,22 @@
+import express from "express"
+import controllers from "../controllers/index.js"
+import middlewares from "../middlewares/index.js"
+
+//
+
+const activitySection = express.Router({ mergeParams: true })
+activitySection.route("/")
+    .get(middlewares.auth.access, controllers.activitySection.get)
+activitySection.route("/section/:secid")
+    .post(middlewares.auth.access, controllers.activitySection.post)
+activitySection.route("/:aseid")
+    .get(middlewares.auth.access, controllers.activitySection.get)
+    .delete(middlewares.auth.access, controllers.activitySection.destroy)
+activitySection.route("/:aseid/section/:secid")
+    .get(middlewares.auth.access, controllers.activitySection.get)
+    .patch(middlewares.auth.access, controllers.activitySection.patch)
+    .delete(middlewares.auth.access, controllers.activitySection.destroy)
+
+//
+
+export default activitySection
--- backend/routes/activity.js
+++ backend/routes/activity.js
@@ -2,11 +2,13 @@ import express from "express"
 import controllers from "../controllers/index.js"
 import middlewares from "../middlewares/index.js"
 import activityEntry from "./activity-entry.js"
+import activitySection from "./activity-section.js"
 
 //
 
 const activity = express.Router()
 activity.use("/:actid/entry", activityEntry)
+activity.use("/:actid/section", activitySection)
 activity.route("/")
     .get(middlewares.auth.access, controllers.activity.get)
     .post(middlewares.auth.access, controllers.activity.post)
