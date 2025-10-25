cad87a0406eb472ff66e741fcff93ec911504ecc
i4nizer <iansandoval264@gmail.com>
2025-10-26 01:09:57 +0800

    Added activity-section on sequelize boot.

--- backend/boot/sequelize.js
+++ backend/boot/sequelize.js
@@ -10,12 +10,15 @@ const run = async () => {
     models.student.Student.init(models.student.attr, models.student.opts(config.sequelize))
     models.activity.Activity.init(models.activity.attr, models.activity.opts(config.sequelize))
     models.activityEntry.ActivityEntry.init(models.activityEntry.attr, models.activityEntry.opts(config.sequelize))
+    models.activitySection.ActivitySection.init(models.activitySection.attr, models.activitySection.opts(config.sequelize))
 
     // --- Assoc
     models.section.Section.hasMany(models.student.Student, { foreignKey: "sectionId", onDelete: "CASCADE" })
     models.student.Student.belongsTo(models.section.Section, { as: "section", foreignKey: "sectionId" })
     models.activity.Activity.hasMany(models.activityEntry.ActivityEntry, { foreignKey: "activityId", onDelete: "CASCADE" })
+    models.activity.Activity.hasMany(models.activitySection.ActivitySection, { foreignKey: "activityId", onDelete: "CASCADE" })
     models.activityEntry.ActivityEntry.belongsTo(models.activity.Activity, { as: "activity", foreignKey: "activityId" })
+    models.activitySection.ActivitySection.belongsTo(models.activity.Activity, { as: "activity", foreignKey: "activityId" })
 
     // --- Connection
     await config.sequelize.authenticate()
