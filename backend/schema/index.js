a61d785287e4f66d3ebef0fb2ae2f816d77fbf90
i4nizer <iansandoval264@gmail.com>
2025-10-25 23:25:37 +0800

    Created activity schema.

--- backend/schema/index.js
+++ backend/schema/index.js
@@ -2,7 +2,8 @@ import user from "./user.js";
 import section from "./section.js";
 import pagination from "./pagination.js";
 import student from "./student.js";
+import activity from "./activity.js";
 
 //
 
-export default { user, section, pagination, student }
+export default { user, section, pagination, student, activity }
