2085bb1389d168f1ee4848ada25d44dfc2258e42
i4nizer <iansandoval264@gmail.com>
2025-10-26 00:02:09 +0800

    Created activity-entry model.

--- backend/models/index.js
+++ backend/models/index.js
@@ -2,7 +2,8 @@ import user from "./user.js";
 import section from "./section.js";
 import student from "./student.js";
 import activity from "./activity.js";
+import activityEntry from "./activity-entry.js";
 
 //
 
-export default { user, section, student, activity }
+export default { user, section, student, activity, activityEntry }
