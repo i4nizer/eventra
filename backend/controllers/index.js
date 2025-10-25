13bc6e1d70d05c564924976f255fe33f22a7ad6f
i4nizer <iansandoval264@gmail.com>
2025-10-26 01:10:15 +0800

    Created activity-section controller.

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -1,9 +1,17 @@
-import activity from "./activity.js";
-import activityEntry from "./activity-entry.js";
-import auth from "./auth.js";
-import section from "./section.js";
-import student from "./student.js";
+import activity from "./activity.js"
+import activityEntry from "./activity-entry.js"
+import activitySection from "./activity-section.js"
+import auth from "./auth.js"
+import section from "./section.js"
+import student from "./student.js"
 
 //
 
-export default { auth, section, student, activity, activityEntry }
+export default {
+	auth,
+	section,
+	student,
+	activity,
+	activityEntry,
+	activitySection,
+}
