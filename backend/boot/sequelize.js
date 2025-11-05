1e836b477e7b347dd8adf1291118416b2e634d46
i4nizer <iansandoval264@gmail.com>
2025-11-06 00:56:40 +0800

    Added attendance to sequelize boot.

--- backend/boot/sequelize.js
+++ backend/boot/sequelize.js
@@ -11,14 +11,19 @@ const run = async () => {
     models.activity.Activity.init(models.activity.attr, models.activity.opts(config.sequelize))
     models.activityEntry.ActivityEntry.init(models.activityEntry.attr, models.activityEntry.opts(config.sequelize))
     models.activitySection.ActivitySection.init(models.activitySection.attr, models.activitySection.opts(config.sequelize))
+    models.attendance.Attendance.init(models.attendance.attr, models.attendance.opts(config.sequelize))
 
     // --- Assoc
     models.section.Section.hasMany(models.student.Student, { foreignKey: "sectionId", onDelete: "CASCADE" })
     models.student.Student.belongsTo(models.section.Section, { as: "section", foreignKey: "sectionId" })
+    models.student.Student.hasMany(models.attendance.Attendance, { foreignKey: "studentId", onDelete: "CASCADE" })
     models.activity.Activity.hasMany(models.activityEntry.ActivityEntry, { foreignKey: "activityId", onDelete: "CASCADE" })
     models.activity.Activity.hasMany(models.activitySection.ActivitySection, { foreignKey: "activityId", onDelete: "CASCADE" })
     models.activityEntry.ActivityEntry.belongsTo(models.activity.Activity, { as: "activity", foreignKey: "activityId" })
+    models.activityEntry.ActivityEntry.hasMany(models.attendance.Attendance, { foreignKey: "activityEntryId", onDelete: "CASCADE" })
     models.activitySection.ActivitySection.belongsTo(models.activity.Activity, { as: "activity", foreignKey: "activityId" })
+    models.attendance.Attendance.belongsTo(models.student.Student, { as: "student", foreignKey: "studentId" })
+    models.attendance.Attendance.belongsTo(models.activityEntry.ActivityEntry, { as: "activityEntry", foreignKey: "activityEntryId" })
 
     // --- Connection
     await config.sequelize.authenticate()
