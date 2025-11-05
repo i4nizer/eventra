a7c923ae589e69a90bfe96ee81406b6987c0b693
i4nizer <iansandoval264@gmail.com>
2025-11-06 01:29:52 +0800

    Created violation schema.

--- backend/schema/index.js
+++ backend/schema/index.js
@@ -6,6 +6,7 @@ import activity from "./activity.js"
 import activityEntry from "./activity-entry.js"
 import activitySection from "./activity-section.js"
 import attendance from "./attendance.js"
+import violation from "./violation.js"
 
 //
 
@@ -18,4 +19,5 @@ export default {
 	activityEntry,
 	activitySection,
 	attendance,
+	violation,
 }
---
+++ backend/schema/violation.js
@@ -0,0 +1,19 @@
+import z from "zod"
+
+//
+
+const ViolationSchema = z.object({
+    id: z.number().int(),
+    fine: z.coerce.number().int(),
+    studentId: z.coerce.number().int(),
+    activityEntryId: z.coerce.number().int(),
+    createdAt: z.coerce.date(),
+    updatedAt: z.coerce.date(),
+})
+
+const ViolationCreateSchema = ViolationSchema.pick({ fine: true })
+const ViolationUpdateSchema = ViolationSchema.pick({ fine: true }).partial()
+
+//
+
+export default { ViolationSchema, ViolationCreateSchema, ViolationUpdateSchema }
