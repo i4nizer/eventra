249c30b397a3593a76674e925fe1380baecd90c8
i4nizer <iansandoval264@gmail.com>
2025-11-20 01:07:45 +0800

    Removed api util.

--- frontend/src/utils/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-    baseURL: `${window.location.protocol}//${window.location.hostname}:4000/`,
-    withCredentials: true
-})
-
-export default api
