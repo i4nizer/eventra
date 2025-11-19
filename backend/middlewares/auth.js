cfc7f7dd6d0fb5b33550f9774e776f92a7ab8fd1
i4nizer <iansandoval264@gmail.com>
2025-11-19 10:06:28 +0800

    Removed header logger.

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -5,7 +5,6 @@ import config from "../config/index.js"
 
 const access = async (req, res, next) => {
     const auth = req.headers.authorization
-    console.info(req.headers)
     if (!auth || !auth.startsWith("Bearer ")) return res.status(401).send("Authorization required.")
     
     const access = auth.split(" ")[1]
