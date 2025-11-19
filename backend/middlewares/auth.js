d3259f16e5ef50fe6714c219a00a9dff7d3840b0
i4nizer <iansandoval264@gmail.com>
2025-11-19 10:00:51 +0800

    Checked auth header instead of cookies in auth middleware.

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,7 +4,11 @@ import config from "../config/index.js"
 //
 
 const access = async (req, res, next) => {
-    const access = req.cookies.access
+    const auth = req.headers.authorization
+    console.info(req.headers)
+    if (!auth || !auth.startsWith("Bearer ")) return res.status(401).send("Authorization required.")
+    
+    const access = auth.split(" ")[1]
     await Promise.resolve()
         .then(() => jwt.verify(access, config.env.jwtAccessSecret))
         .then((payload) => req.payload = payload)
