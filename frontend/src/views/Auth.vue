f42210f7486ad0797e2c83a8409a427a17a74bff
i4nizer <iansandoval264@gmail.com>
2025-11-19 09:59:15 +0800

    Implemented auth with api composable.

--- frontend/src/views/Auth.vue
+++ frontend/src/views/Auth.vue
@@ -16,36 +16,25 @@
 <script setup>
 import SignUpForm from '@/components/Auth/SignUpForm.vue';
 import SignInForm from '@/components/Auth/SignInForm.vue';
-import api from '@/utils/api';
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
+import { useApi } from '@/composables/api';
 
 //
 
 const router = useRouter()
+const { signUp, signIn } = useApi()
 const isSignUp = ref(false)
 
-const onSignUp = async (name, email, password) => {
-  const res = await api.post("/api/auth/sign-up", { name, email, password })
+//
 
-  if (!res.ok) return console.info(await res.text())
-  const user = await res.json()
-  console.info("User signed-up: ", user)
+const onSignUp = async (name, email, password) => {
+  const user = await signUp(name, email, password)
 }
 
 const onSignIn = async (email, password) => {
-  const res = await api.post("/api/auth/sign-in", { email, password })
-
-  if (!res.ok) return console.info(await res.text())
-  const user = await res.json()
-  console.info("User signed-in: ", user)
-
+  await signIn(email, password)
   router.push("/dashboard")
-  /**
-   * Access cookie is attached to the browser marking user as signed-in.
-   * Do I need to manually manage signed-in state? No
-   * Am I detected signed-in wherever whenever I used fetch? Yes, cookie is attached to every request.
-   */
 }
 
 //
