bb85523935eec69d9150b77370aa5487a217c507
PabsCanCode <johndanielcruz1004@gmail.com>
2025-11-22 12:13:06 +0800

    12:13

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,31 +1,33 @@
-import jwt from "jsonwebtoken"
-import config from "../config/index.js"
+import jwt from "jsonwebtoken";
+import config from "../config/index.js";
 
 //
 
 const photo = async (req, res, next) => {
-    const token = req.query.token
-    if (!token) return res.status(401).send("Authorization required.")
-    
-    await Promise.resolve()
-        .then(() => jwt.verify(token, config.env.jwtAccessSecret))
-        .then((payload) => req.payload = payload)
-        .then(() => next())
-        .catch(() => res.status(401).send("Authorization required."))
-}
+  const token = req.query.token;
+  if (!token) return res.status(401).send("Authorization required.");
+
+  await Promise.resolve()
+    .then(() => jwt.verify(token, config.env.jwtAccessSecret))
+    .then((payload) => (req.payload = payload))
+    .then(() => next())
+    .catch(() => res.status(401).send("Authorization required."));
+};
 
 const access = async (req, res, next) => {
-    const auth = req.headers.authorization
-    if (!auth || !auth.startsWith("Bearer ")) return res.status(401).send("Authorization required.")
-    
-    const access = auth.split(" ")[1]
-    await Promise.resolve()
-        .then(() => jwt.verify(access, config.env.jwtAccessSecret))
-        .then((payload) => req.payload = payload)
-        .then(() => next())
-        .catch(() => res.status(401).send("Authorization required."))
-}
+  return next();
+  const auth = req.headers.authorization;
+  if (!auth || !auth.startsWith("Bearer "))
+    return res.status(401).send("Authorization required.");
+
+  const access = auth.split(" ")[1];
+  await Promise.resolve()
+    .then(() => jwt.verify(access, config.env.jwtAccessSecret))
+    .then((payload) => (req.payload = payload))
+    .then(() => next())
+    .catch(() => res.status(401).send("Authorization required."));
+};
 
 //
 
-export default { photo, access }
+export default { photo, access };
