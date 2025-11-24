0dde24373b24956741d8701954b57dd23a59570e
i4nizer <iansandoval264@gmail.com>
2025-11-24 16:45:20 +0800

    Added id array on pagination schema.

--- backend/schema/pagination.js
+++ backend/schema/pagination.js
@@ -3,6 +3,7 @@ import z from "zod"
 //
 
 const PaginationSchema = z.object({
+	idarr: z.union([z.string(), z.array(z.string())]).transform((v) => Array.isArray(v) ? v : [v]),
 	alpha: z.coerce.date(),
 	omega: z.coerce.date(),
 	limit: z.coerce.number().int(),
