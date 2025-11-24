c719c142babf68e2d0e881d902b83d9e78e7d9b0
i4nizer <iansandoval264@gmail.com>
2025-11-24 22:20:48 +0800

    Added delete handler for voiding attendance.

--- backend/controllers/esp/attendance.js
+++ backend/controllers/esp/attendance.js
@@ -32,7 +32,7 @@ const post = async (req, res) => {
     const section = await models.activitySection.ActivitySection.findOne({ where: secfil })
     if (!section) return res.status(400).send("Student is not on the list.")
     
-    // --- Check if the student already have an attendance entry
+    // --- Check if the student already have an attendance
     const attdata = { entryId: entry.id, studentId: student.id }
     const attendance = await models.attendance.Attendance.findOne({ where: attdata })
     if (attendance) return res.status(403).send("Student already have an attendance to this event.")
@@ -41,6 +41,38 @@ const post = async (req, res) => {
     return res.send(`Attendance recorded successfully for ${student.name}.`)
 }
 
+const destroy = async (req, res) => {
+    const { eveid, rfid } = req.params
+    if (!rfid) return res.status(404).send("Student's rfid required.")
+    if (!eveid) return res.status(400).send("Event id required.")
+
+	// --- Find event
+	const event = await models.activity.Activity.findByPk(eveid)
+    if (!event) return res.status(404).send("Event not found.")
+    
+    // --- Check event expiry
+    const expired = Date.now() > event.dataValues.finishAt.getTime()
+    if (expired) return res.status(400).send("Event already expired.")
+    
+    // --- Find active event entry
+    const entfil = { activityId: event.id, startAt: { [Op.lte]: new Date() }, finishAt: { [Op.gte]: new Date() } }
+    const entry = await models.activityEntry.ActivityEntry.findOne({ where: entfil })
+    if (!entry) return res.status(400).send(`Event "${event.name}" currently doesn't accept attendance.`)
+	
+    // --- Find student using rfid
+	const student = await models.student.Student.findOne({ where: { rfid } })
+    if (!student) return res.status(404).send("Student not registered.")
+    
+    // --- Check if the student have an attendance
+    const attdata = { entryId: entry.id, studentId: student.id }
+    const attendance = await models.attendance.Attendance.findOne({ where: attdata })
+    if (!attendance) return res.status(404).send("Student doesn't have attendance to this event.")
+
+    // --- Delete attendance
+    await attendance.destroy()
+    return res.send(`Attendance deleted successfully for ${student.name}.`)
+}
+
 //
 
-export default { post }
+export default { post, destroy }
